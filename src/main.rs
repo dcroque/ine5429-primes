d87750c531d32092b23a8b303c73530d37b1dcad
Matheus D C Roque <matheusdcroque@gmail.com>
2021-07-14 22:11:38 -0300

    Add Lagged Fibonacci and document MLCG

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ use num_bigint::{BigUint, ToBigUint};
 fn main() {
     //TODO: Achar raizes primitivas de 2^4253-1, como candidato a bom argumento de multiplicador
     let mlcg_mult: BigUint = 16807.to_biguint().unwrap();
-    let mut mlcg_gen: Mlcg = Mlcg::new_mersene(mlcg_mult, 4253);
+    let mut mlcg_gen: Mlcg = Mlcg::new_mersene(mlcg_mult, 4253, 40);
 
-    println!("{:b}\n", mlcg_gen.rand_sized(40));
+    println!("{:b}\n", mlcg_gen.rand());
+
+    let ini_vec = Vec::new();
+    let lf_mod = 2.to_biguint().unwrap().pow(4253);
+
+    let mut lf_gen = LaggedFibonacci::new(ini_vec, 418, 1279, lf_mod, std::ops::Mul::mul, 64);
+
+    for _ in 1 .. 30 {
+        println!("{:b}\n", lf_gen.rand());
+    }
 }
--- src/rand_gen.rs
+++ src/rand_gen.rs
@@ -2,16 +2,23 @@ use num_bigint::{BigUint, ToBigUint};
 use std::ops::{BitAnd, BitOr};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-// TODO: Documentar classe e implementações
+/// Multiplicative linear congruential generator, também conhecido como Park-Miller RNG. Calcula novos valores por meio da fórmula s = mu*s % mo, sendo _s_ o ultimo valor gerado (ou inicialmente a semente), _mu_ um multiplicador e _mo_ o modulo do gerador. Recomenda-se que _mu_ e _mo_ sejam pelo menos coprimos.
 pub struct Mlcg {
+    /// Corresponde ao último numero gerado pela estrutura.
     state: BigUint,
+    /// Semente do gerador
     seed: BigUint,
+    /// Fator que multiplica o valor atual na geração do próximo.
     mult_factor: BigUint,
+    /// Valor pelo qual é realizada a divisão modular na geração do próximo valor.
     mod_factor: BigUint,
+    /// Tamanho dos valores gerados
+    size: u32,
 }
 
 impl Mlcg {
-    pub fn new(mu: BigUint, mo: BigUint) -> Self {
+    /// Constrói um novo MLCG com multiplicador = _mu_, modulo = _mo_, semente gerada com base no horário local e tamanho de _size_ bits.
+    pub fn new(mu: BigUint, mo: BigUint, size: u32) -> Self {
         let seed: BigUint = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
@@ -24,19 +31,23 @@ impl Mlcg {
             seed,
             mult_factor: mu,
             mod_factor: mo,
+            size,
         }
     }
 
-    pub fn new_from_seed(s: BigUint, mu: BigUint, mo: BigUint) -> Self {
+    /// Constrói um novo MLCG com multiplicador = _mu_, modulo = _mo_, semente = _s_ e tamanho de _size_ bits.
+    pub fn new_from_seed(mu: BigUint, mo: BigUint, size: u32, s: BigUint) -> Self {
         Mlcg {
             state: Mlcg::warm_up(&s, &mu, &mo),
             seed: s,
             mult_factor: mu,
             mod_factor: mo,
+            size,
         }
     }
 
-    pub fn new_mersene(mu: BigUint, mer: u32) -> Self {
+    /// Constrói um novo MLCG com multiplicador = _mu_, modulo = 2^_mer_-1, semente gerada com base no horário local e tamanho de _size_ bits.
+    pub fn new_mersene(mu: BigUint, mer: u32, size: u32) -> Self {
         let seed: BigUint = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
@@ -50,10 +61,12 @@ impl Mlcg {
             seed,
             mult_factor: mu,
             mod_factor: mo,
+            size,
         }
     }
 
-    pub fn new_mersene_from_seed(s: BigUint, mu: BigUint, mer: u32) -> Self {
+    /// Constrói um novo MLCG com multiplicador = _mu_, modulo = 2^_mer_-1, semente = _s_ e tamanho de _size_ bits.
+    pub fn new_mersene_from_seed(mu: BigUint, mer: u32, size: u32, s: BigUint) -> Self {
         let mo = BigUint::pow(&2.to_biguint().unwrap(), mer) - 1.to_biguint().unwrap();
 
         Mlcg {
@@ -61,34 +74,162 @@ impl Mlcg {
             seed: s,
             mult_factor: mu,
             mod_factor: mo,
+            size,
         }
     }
 
+    /// Retorna o último valor pseudo aleatório gerado pela estrutura.
     pub fn value(&self) -> BigUint {
         self.state.clone()
     }
 
+    /// Retorna a semente da estrutura.
     pub fn seed(&self) -> BigUint {
         self.seed.clone()
     }
 
-    pub fn rand(&mut self) -> BigUint {
+    /// Retorna o pŕoximo valor pseudo aleatório calculado pela estrutura.
+    fn calculate_next(&mut self) -> BigUint {
         self.state = (&self.state * &self.mult_factor) % &self.mod_factor;
         self.value()
     }
 
-    pub fn rand_sized(&mut self, size: u64) -> BigUint {
-        let temp = &self.rand();
-        let msb: BigUint = 1.to_biguint().unwrap() << (size - 1);
-        let ones: BigUint = (1.to_biguint().unwrap() << size) - 1.to_biguint().unwrap();
+    /// Retorna o pŕoximo valor pseudo aleatório gerado pela estrutura com um tamanho específicado de bits.
+    pub fn rand(&mut self) -> BigUint {
+        let temp = &self.calculate_next();
+        let msb: BigUint = 1.to_biguint().unwrap() << (self.size - 1);
+        let ones: BigUint = (1.to_biguint().unwrap() << self.size) - 1.to_biguint().unwrap();
         temp.bitand(ones).bitor(msb)
     }
 
+    /// Realiza um aquecimento na semente para evitar que os valores iniciais correspondam à semente e/ou exponham o multiplicador utilizado.
     fn warm_up(s: &BigUint, mu: &BigUint, mo: &BigUint) -> BigUint {
         if (s << 1) < *mo {
             Mlcg::warm_up(&(s * mu % mo), mu, mo)
         } else {
             s.clone()
         }
+    }
+}
+
+pub struct LaggedFibonacci<T>
+where
+    T: Fn(BigUint, BigUint) -> BigUint,
+{
+    /// Operação realizada entre os valores n-j e n-k, como por exemplo Mul::mul, Add::add ou BitXor::bitxor
+    operation: T,
+    /// Semente do gerador
+    seed: BigUint,
+    /// Histórico de valores gerados/inseridos, para geração de novos
+    states: Vec<BigUint>,
+    /// Indicação de antecessor mais recente para utilizar na geração do próximo elemento
+    ele_j: usize,
+    /// Indicação de antecessor menos recente para utilizar na geração do próximo elemento
+    ele_k: usize,
+    /// Valor pelo qual é realizada a divisão modular na geração do próximo valor.
+    mod_factor: BigUint,
+    /// Tamanho dos valores gerados
+    size: u32,
+}
+
+impl<T> LaggedFibonacci<T>
+where
+    T: Fn(BigUint, BigUint) -> BigUint,
+{
+    /// Constrói um novo Lagged-Fibonacci RNG com módulo _mo_ que realiza a operação _op_ entre os elementos n-_j_ e n-_k_, com lista inicial de elementos _elements_.
+    pub fn new(elements: Vec<BigUint>, j: u16, k: u16, mo: BigUint, op: T, size: u32) -> Self {
+        let seed: BigUint = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_biguint()
+            .unwrap();
+
+        let mut temp = LaggedFibonacci {
+            operation: op,
+            seed,
+            states: elements,
+            ele_j: j.into(),
+            ele_k: k.into(),
+            mod_factor: mo,
+            size,
+        };
+        temp.check_initialization();
+        temp
+    }
+
+    /// Constrói um novo Lagged-Fibonacci RNG com módulo _mo_ que realiza a operação _op_ entre os elementos n-_j_ e n-_k_.
+    pub fn new_from_seed(
+        elements: Vec<BigUint>,
+        j: u16,
+        k: u16,
+        mo: BigUint,
+        op: T,
+        size: u32,
+        s: BigUint,
+    ) -> Self {
+        let mut temp = LaggedFibonacci {
+            operation: op,
+            seed: s,
+            states: elements,
+            ele_j: j.into(),
+            ele_k: k.into(),
+            mod_factor: mo,
+            size,
+        };
+        temp.check_initialization();
+        temp
+    }
+
+    /// Checa e corrige problemas referentes aos valores iniciais fornecidos para a inicialização da estrutura
+    fn check_initialization(&mut self) {
+        if self.ele_j > self.ele_k {
+            let temp = self.ele_j;
+
+            self.ele_j = self.ele_k;
+            self.ele_k = temp;
+        } else if self.ele_j == self.ele_k {
+            self.ele_j -= 1;
+        }
+
+        if self.states.len() > self.ele_k {
+            self.states.truncate(self.ele_k);
+        } else if self.states.len() < self.ele_k {
+            let mut temp = Mlcg::new_mersene_from_seed(
+                16087.to_biguint().unwrap(),
+                31,
+                self.size,
+                self.seed(),
+            );
+            while self.states.len() < self.ele_k {
+                self.states.insert(0, temp.rand() | 1.to_biguint().unwrap())
+            }
+        }
+    }
+
+    /// Retorna o último valor pseudo aleatório gerado pela estrutura.
+    pub fn value(&self) -> BigUint {
+        self.states.last().unwrap().clone()
+    }
+
+    /// Retorna a semente da estrutura.
+    pub fn seed(&self) -> BigUint {
+        self.seed.clone()
+    }
+
+    /// Retorna o pŕoximo valor pseudo aleatório gerado pela estrutura.
+    fn calculate_next(&mut self) -> BigUint {
+        let ele_j: BigUint = self.states[self.states.len() - self.ele_j].clone();
+        let ele_k: BigUint = self.states.remove(0);
+        self.states.push((self.operation)(ele_j, ele_k) % &self.mod_factor);
+        self.value()
+    }
+
+    /// Retorna o pŕoximo valor pseudo aleatório gerado pela estrutura com um tamanho específicado de bits.
+    pub fn rand(&mut self) -> BigUint {
+        let temp = &self.calculate_next();
+        let msb: BigUint = 1.to_biguint().unwrap() << (self.size - 1);
+        let ones: BigUint = (1.to_biguint().unwrap() << self.size) - 1.to_biguint().unwrap();
+        temp.bitand(ones).bitor(msb)
     }
 }
